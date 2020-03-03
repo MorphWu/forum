2f96ab6583311be310e07f29fda8bb6112f1108d
MorphWu <fwyd1994163@example.com>
2020-03-03 19:15:08 +0800

    1.0

---
+++ src/main/java/com/morphforum/demo/FourmdemoApplication.java
@@ -0,0 +1,13 @@
+package com.morphforum.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourmdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morphforum/demo/FourmdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morphforum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourmdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
